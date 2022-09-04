ef909b6f5013692ab9f2376d8fc2b0ef96313e42
Bitemake <turbovtop@gmail.com>
2022-09-04 11:52:36 +0300

    again modify

--- proj1.py
+++ proj1.py
@@ -1,5 +1,6 @@
+print("hello, maybe world")
 first_name = "Bite"
 last_name = "make"
 full_name = first_name + last_name
 age = 20
-print("Hi, me " + full_name + "." + " I am 20 years old " + "and i slacker.")
+print("Me " + full_name + "." + " I am 20 years old " + "and i slacker.")
