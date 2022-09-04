bc2263fe0c020762e1ef5cb31460f053008080cc
Bitemake <turbovtop@gmail.com>
2022-09-04 11:46:53 +0300

    first project

---
+++ main.py
@@ -0,0 +1,5 @@
+print("hello bitch")
+first_name = "Jack"
+last_name = "Night"
+full_name = first_name + last_name
+print(full_name)
---
+++ proj1.py
@@ -0,0 +1,5 @@
+first_name = "Bite"
+last_name = "make"
+full_name = first_name + last_name
+age = 20
+print("Hi, me " + full_name + "." + " I am 20 years old " + "and i slacker.")
